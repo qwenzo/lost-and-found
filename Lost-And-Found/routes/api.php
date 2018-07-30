0bda1d230a50f544c5b39e979d39756f7b826b3d
Hazem abd el aleam <hazem29778865@gmail.com>
2018-07-30 10:28:14 +0200

    Route Documentd in api file

--- Lost-And-Found/routes/api.php
+++ Lost-And-Found/routes/api.php
@@ -18,3 +18,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::resource('item','ItemController');
+/*
+           GET|HEAD  | api/item             | item.index   | Show all items   | api
+|        | POST      | api/item             | item.store   | Store Item to the data base   | api
+|        | GET|HEAD  | api/item/create      | item.create  | redirect to item creation page  | api
+|        | GET|HEAD  | api/item/{item}      | item.show    | show specific item    | api          |
+|        | PUT|PATCH | api/item/{item}      | item.update  | Update specific item in the DataBase  | api
+|        | DELETE    | api/item/{item}      | item.destroy | Delete specific item  | api
+|        | GET|HEAD  | api/item/{item}/edit | item.edit    | Redirect to Edit Page    | api
+*/
